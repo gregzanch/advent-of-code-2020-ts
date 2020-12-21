acd1730710ac5567c8fadbf05774ee00f48babdd
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-12-21 03:05:03 -0500

    update various helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,27 @@
 import fs, { readFileSync } from "fs";
 import path from "path";
 
-type ReadPuzzleInputProps = { day: number; seperator?: string };
-export function readPuzzleInput({ day, seperator }: ReadPuzzleInputProps) {
+export function readInputs<T extends ReadInputProps>(
+  props: T
+): T extends { seperator: string }
+  ? {
+      input: string[];
+      example: string[];
+    }
+  : {
+      input: string;
+      example: string;
+    };
+
+export function readInputs({ day, seperator }) {
+  return {
+    input: readPuzzleInput({ day, seperator }),
+    example: readExampleInput({ day, seperator }),
+  };
+}
+
+type ReadInputProps = { day: number; seperator?: string };
+export function readPuzzleInput({ day, seperator }: ReadInputProps) {
   const fileContents = readFileSync(`src/day${day}/input.txt`, "utf8");
   if (typeof seperator !== "undefined") {
     return fileContents.split(seperator);
@@ -11,12 +30,19 @@ export function readPuzzleInput({ day, seperator }: ReadPuzzleInputProps) {
   }
 }
 
+export function readExampleInput({ day, seperator }: ReadInputProps) {
+  const fileContents = readFileSync(`src/day${day}/example.txt`, "utf8");
+  if (typeof seperator !== "undefined") {
+    return fileContents.split(seperator);
+  } else {
+    return fileContents;
+  }
+}
+
 export const getIndex = (i: string | number) => (x: any) => x[i];
-export const ascending = <T extends number | string>(a: T, b: T) => +a - +b;
-export const descending = <T extends number | string>(a: T, b: T) => +b - +a;
-
+export const ascending = <T extends number | string>(a: T, b: T) => (a < b ? -1 : +1);
+export const descending = <T extends number | string>(a: T, b: T) => (a < b ? +1 : -1);
 export const via = (i: string | number) => (fn: Function) => (...args: any[][]) => fn(...args.map(getIndex(i)));
-
 export const applyAt = (i: number) => (fn: Function) => <T>(x: T[]) => fn(x[i]);
 export const match = (expr: string | RegExp) => (str: string) => str.match(expr);
 
@@ -28,20 +54,12 @@ export const swap = (a: number, b: number) => <T>(arr: T[]) => [
   ...arr.slice(b + 1),
 ];
 
-export const reducers = {
-  sum: (a: number, b: number) => a + b,
-  diff: (a: number, b: number) => a + b,
-  max: (a: number, b: number) => (a > b ? a : b),
-  min: (a: number, b: number) => (a < b ? a : b),
-};
 export const add = (a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
 export const multiply = (a: number, b: number) => a * b;
-
-export const filters = {
-  truthy: (x: any) => x,
-  falsey: (x: any) => !x,
-};
+export const divide = (a: number, b: number) => a / b;
+export const max = (a: number, b: number) => (a > b ? a : b);
+export const min = (a: number, b: number) => (a < b ? a : b);
 
 export const truthy = (x: any) => !!x;
 export const falsey = (x: any) => !x;
@@ -58,18 +76,34 @@ export const toIndexed = <T>(x: T, i: number, a: T[]): [number, T] => [i, x];
 
 export const regularExpressions = {
   hexColor: /\#[a-z0-9]{6}/gim,
+  comma: /,/g,
+  digit: /\d/g,
+  digits: /\d+/g,
 };
 
-export const comma = /,/g;
-export const digit = /\d/g;
-export const digits = /\d+/g;
+export function splitEvery(arr: Array<any>, n: number) {
+  return arr.reduce(
+    (a, b) => {
+      if (a[a.length - 1].length < n) {
+        a[a.length - 1].push(b);
+      } else {
+        a.push([b]);
+      }
+      return a;
+    },
+    [[]]
+  );
+}
+
+export const multiMap = (fn) => (a, b) => a.map((x, i) => fn(x, b[i]));
+
+export const addArray = multiMap(add);
+export const subtractArray = multiMap(subtract);
+export const multiplyArray = multiMap(multiply);
 
 export const toNumbers = <T extends string | number | boolean>(x: T[]) => x.map(Number);
-
 export const between = (v: number, a: number, b: number) => v >= a && v <= b;
-
 export const unique = (arr: Iterable<any>) => Array.from(new Set(arr));
-
 export const diff = (A: any[], B: any[]) => {
   return [A, B].map((x, i, a) => x.filter((y) => !a[(i + 1) % 2].includes(y)));
 };
@@ -169,3 +203,96 @@ export class GraphNode<T> implements GraphNode<T> {
     return this.children;
   }
 }
+
+export interface Range {
+  [0]: number;
+  [1]: number;
+}
+
+export class Range implements Range {
+  constructor(min: number, max: number) {
+    this[0] = Math.min(min, max);
+    this[1] = Math.max(min, max);
+  }
+  get min() {
+    return this[0];
+  }
+  set min(m: number) {
+    if (m > this[1]) {
+      console.warn(`new value (${m}) for min (${this[0]}) is greater than max ${this[1]}, switching automatically`);
+      this[0] = this[1];
+      this[1] = m;
+    } else {
+      this[0] = m;
+    }
+  }
+
+  get max() {
+    return this[1];
+  }
+  set max(m: number) {
+    if (m < this[0]) {
+      console.warn(`new value (${m}) for max (${this[1]}) is less than min ${this[0]}, switching automatically`);
+      this[1] = this[0];
+      this[0] = m;
+    } else {
+      this[1] = m;
+    }
+  }
+
+  get delta() {
+    return this[1] - this[0];
+  }
+
+  contains(val: number) {
+    return val > this.min && val < this.max;
+  }
+}
+
+export class ClosedRange extends Range {
+  contains(val: number) {
+    return val >= this.min && val <= this.max;
+  }
+}
+
+interface ReversedArray<T> {
+  [index: number]: T;
+}
+
+export function* getAdjacentIterator(n: number, range: [number, number]) {
+  const m = range[1] - range[0] + 1;
+  for (let i = 0; i < m ** n; i++) {
+    yield i
+      .toString(m)
+      .padStart(n, "0")
+      .split("")
+      .map((x) => Number(x) + range[0]);
+  }
+  return;
+}
+
+function* fibonacci() {
+  let current = 0;
+  let next = 1;
+  while (true) {
+    let reset = yield current;
+    [current, next] = [next, next + current];
+    if (reset) {
+      current = 0;
+      next = 1;
+    }
+  }
+}
+
+const sequence = fibonacci();
+sequence.next().value;
+sequence.next().value;
+sequence.next().value;
+sequence.next().value;
+sequence.next().value;
+sequence.next().value;
+sequence.next().value;
+sequence.next(true).value;
+sequence.next().value;
+sequence.next().value;
+sequence.next().value;
