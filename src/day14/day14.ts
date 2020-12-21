0bb986f34395bc9bf3c6e9938a4e28723e67f2dc
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-12-21 03:05:26 -0500

    add solution for day 14

--- src/day14/day14.ts
+++ src/day14/day14.ts
@@ -1,179 +1,181 @@
-import { comma, readPuzzleInput } from "../helpers";
+import { sum } from "ramda";
+import { readPuzzleInput } from "../helpers";
 
+// read the input file
 const input = readPuzzleInput({ day: 14, seperator: "\n" }) as string[];
 
+// declare
 type Command = "mask" | "mem";
 
-type Instruction<T extends Command> =
-  | {
-      cmd: "mask";
-      value: string[];
-      original: string;
-    }
-  | {
-      cmd: "mem";
-      value: [number, number];
-      original: string;
-    };
+type BinaryBit = "0" | "1";
+type FloatingBit = "X";
+type MaskBit = BinaryBit | FloatingBit;
 
-const parseInstruction = (str: string): Instruction<Command> => {
+type MaskedBinaryRep = Array<MaskBit>;
+type BinaryRep = Array<BinaryBit>;
+
+type SetMaskInstruction = {
+  cmd: "mask";
+  value: MaskedBinaryRep;
+  original: string;
+};
+
+type WriteMemoryInstruction = {
+  cmd: "mem";
+  address: number;
+  value: number;
+  original: string;
+};
+
+type Instruction = SetMaskInstruction | WriteMemoryInstruction;
+
+const convertNumberToBinaryRep = (bitLength: number) => {
+  return (value: number): BinaryRep => {
+    return value.toString(2).padStart(bitLength, "0").split("") as BinaryRep;
+  };
+};
+
+const convertBinaryRepToNumber = (binaryRep: BinaryRep) => {
+  return parseInt(binaryRep.join(""), 2);
+};
+
+const sumMap = <T>(map: Map<T, number>) => {
+  let sum = 0;
+  map.forEach((val) => {
+    sum += val;
+  });
+  return sum;
+};
+
+const parseInstruction = (str: string): Instruction => {
   const [lhs, rhs] = str.split(" = ");
   if (lhs.startsWith("mask")) {
     return {
       cmd: "mask",
       value: rhs.split(""),
       original: str,
-    };
+    } as SetMaskInstruction;
   } else {
     return {
       cmd: "mem",
-      value: [Number(lhs.replace(/(mem\[)|(\])/gim, "")), Number(rhs)],
+      address: Number(lhs.replace(/(mem\[)|(\])/gim, "")),
+      value: Number(rhs),
       original: str,
-    };
+    } as WriteMemoryInstruction;
   }
 };
 
-function part1(input: string[]) {
-  const memory = new Map<number, number[]>();
-  const commandStream = input.map(parseInstruction);
+type MaskRuleset<T extends MaskBit | BinaryBit> = {
+  [bit in MaskBit]: (targetBit: BinaryBit) => T;
+};
 
-  let currentMask = commandStream[0].value as string[];
+const composeMaskFromRuleset = <T extends MaskBit | BinaryBit>(ruleset: MaskRuleset<T>) => {
+  return (mask: MaskedBinaryRep) => {
+    return (target: BinaryRep) => {
+      return mask.map((maskBit: MaskBit, index: number) => {
+        return ruleset[maskBit](target[index]);
+      });
+    };
+  };
+};
+
+function part1(input: string[]) {
+  const memory = new Map<number, number>();
+  const commandStream = input.map(parseInstruction);
+  const composeMask = composeMaskFromRuleset({
+    0: (targetBit) => "0",
+    1: (targetBit) => "1",
+    X: (targetBit) => targetBit,
+  });
+  let currentMask: MaskedBinaryRep = (commandStream[0] as SetMaskInstruction).value;
+  let applyMask = composeMask(currentMask);
 
   commandStream.forEach((command) => {
-    switch (command[0]) {
+    switch (command.cmd) {
       case "mask":
-        {
-          currentMask = command[1];
-        }
+        currentMask = command.value;
+        applyMask = composeMask(currentMask);
         break;
       case "mem":
-        {
-          const bin = command[1][1].toString(2).padStart(36, "0").split("").map(Number); //?
-          const masked = currentMask.map((x, i) => {
-            if (x === "X") {
-              return bin[i];
-            } else {
-              return Number(x);
-            }
-          });
-          memory.set(command[1][0], masked);
-        }
+        const { address, value } = command;
+        const valueAsBinaryRep = convertNumberToBinaryRep(36)(value);
+        const maskedValue = applyMask(valueAsBinaryRep);
+        const maskedValueAsNumber = convertBinaryRepToNumber(maskedValue);
+        memory.set(address, maskedValueAsNumber);
         break;
       default:
         break;
     }
   });
-  let sum = 0;
-  memory.forEach((val, addr) => {
-    sum += parseInt(val.map(String).join(""), 2);
-  });
 
-  return sum;
+  return sumMap(memory);
 }
 
-const dec2binArray = (n: number) => (v: number): number[] => v.toString(2).padStart(n, "0").split("").map(Number);
-
-function* iterateOverString(str: string) {
-  for (let i = 0; i < str.length; i++) {
-    yield str[i] as string;
+function* iterateOverString(binaryRep: BinaryRep) {
+  for (let i = 0; i < binaryRep.length; i++) {
+    yield binaryRep[i] as BinaryBit;
   }
-  return str;
+  return binaryRep[0] as BinaryBit;
 }
 
 function part2(input: string[]) {
-  const memory = new Map<string[], number>();
-  const memoryDecimal = new Map<number, number>();
-  const memoryString = new Map<string, number>();
+  const memory = new Map<string, number>();
+  const memory2 = new Map<number, number>();
   const commandStream = input.map(parseInstruction);
+  const composeMask = composeMaskFromRuleset({
+    0: (targetBit) => targetBit,
+    1: (targetBit) => "1",
+    X: (targetBit) => "X",
+  });
 
-  const maskAddress = (originalBinaryAddress: string[] | number[]) => (bit: string, index: number) => {
-    // If the bitmask bit is 0, the corresponding memory address bit is unchanged.
-    if (bit === "0") {
-      return String(originalBinaryAddress[index]);
-    }
-    // If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1.
-    if (bit === "1") {
-      return "1";
-    }
-    // If the bitmask bit is X, the corresponding memory address bit is floating
-    if (bit === "X") {
-      return "X";
-    }
-    return;
-  };
-
-  const setFloatingBits = (maskedAddress: string[]) => (combination: string) => {
+  const setFloatingBits = (maskedAddress: MaskedBinaryRep) => (combination: BinaryRep): BinaryRep => {
     const combinationBitIterator = iterateOverString(combination);
-    return maskedAddress.map((bit) => {
-      if (bit === "X") {
-        const { value, done } = combinationBitIterator.next();
-        if (!done) {
-          return value;
-        }
+    const binary: BinaryRep = [];
+    for (let i = 0; i < maskedAddress.length; i++) {
+      if (maskedAddress[i] === "X") {
+        binary.push(combinationBitIterator.next().value);
+      } else if (maskedAddress[i] === "1") {
+        binary.push("1");
+      } else {
+        binary.push("0");
       }
-      return bit;
-    });
+    }
+    return binary;
   };
 
-  const getFloatingBitCount = (binaryAddress: string[]) => {
+  const getFloatingBitCount = (binaryAddress: MaskedBinaryRep) => {
     return binaryAddress.filter((x) => x === "X").length;
   };
 
-  const sumMap = <T>(map: Map<T, number>) => {
-    let sum = 0;
-    map.forEach((val) => {
-      sum += val;
-    });
-    return sum;
-  };
+  let currentMask: MaskedBinaryRep;
+  let applyMask: (target: BinaryRep) => MaskBit[];
 
-  let currentMask = commandStream[0].value as string[];
   commandStream.forEach((command) => {
-    console.log(command.original);
     switch (command.cmd) {
       case "mask":
-        {
-          currentMask = command.value;
-        }
+        currentMask = command.value;
+        applyMask = composeMask(currentMask);
         break;
       case "mem":
-        {
-          const orginalAddress = command.value[0];
-          const originalBinaryAddress = dec2binArray(36)(orginalAddress);
-          const maskedAddress = currentMask.map(maskAddress(originalBinaryAddress));
-          const count = getFloatingBitCount(maskedAddress);
-          const getNewBinaryAddress = setFloatingBits(maskedAddress);
-          // for each combination of floating bits
-          for (let i = 0; i < 2 ** count; i++) {
-            // get binary rep
-            let comb = i.toString(2).padStart(count, "0");
-            const newBinaryAddress = getNewBinaryAddress(comb);
+        const { address, value } = command;
+        const originalBinaryAddress = convertNumberToBinaryRep(36)(address);
+        const maskedAddress = applyMask(originalBinaryAddress);
+        const count = getFloatingBitCount(maskedAddress);
+        const getNewBinaryAddress = setFloatingBits(maskedAddress);
 
-            memory.set(newBinaryAddress, command.value[1]);
-            memoryString.set(newBinaryAddress.join(""), command.value[1]);
-            memoryDecimal.set(parseInt(newBinaryAddress.join(""), 2), command.value[1]);
-          }
+        for (let i = 0; i < 2 ** count; i++) {
+          const brep = convertNumberToBinaryRep(count)(i);
+          const newBinaryAddress = getNewBinaryAddress(brep);
+          memory.set(newBinaryAddress.join(""), value);
+          memory2.set(convertBinaryRepToNumber(newBinaryAddress), value);
         }
         break;
       default:
         break;
     }
   });
-  const sums = [];
 
-  return {
-    memory: sumMap(memory),
-    memoryDecimal: sumMap(memoryDecimal),
-    memoryString: sumMap(memoryString),
-  };
+  return sumMap(memory);
 }
 
-// part1(input); //?
-
-const part2ex = `mask = 000000000000000000000000000000X1001X
-mem[42] = 100
-mask = 00000000000000000000000000000000X0XX
-mem[26] = 1`;
-
-console.log(part2(input)); //?
-// part2(part2ex.split("\n")); //?
+console.log(part2(input));
